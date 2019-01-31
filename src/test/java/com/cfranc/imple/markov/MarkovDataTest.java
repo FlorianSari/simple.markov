a2507ac5e08f085a781ed46d08f442607a10ea7c
FlorianSari <47080860+FlorianSari@users.noreply.github.com>
2019-01-31 16:42:30 +0100

    Add files via upload

--- src/test/java/com/cfranc/imple/markov/MarkovDataTest.java
+++ src/test/java/com/cfranc/imple/markov/MarkovDataTest.java
@@ -7,8 +7,58 @@ import org.junit.Test;
 public class MarkovDataTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testRenforceWord() {
+		MarkovData m = new MarkovData("Il était une fois");
+               assertEquals(false, m.renforceWord("Il", "était") );            
 	}
+        
+        @Test
+        public void testRenforceWordTrue() {
+            MarkovData m = new MarkovData("Il était une fois");
+            assertEquals(false, m.renforceWord("Il", "était") );
+        }
 
+        @Test
+        public void testGetLearnedWordTrue(){
+            MarkovData m = new MarkovData("Il était une fois");
+            m.learnWord("était", "une");
+            assertEquals("une",m.getLearnedWord("était"));
+        }
+        
+        @Test
+        public void testGetLearnedWordFalse(){
+            MarkovData m = new MarkovData("Il était une fois");
+            m.learnWord("était", "une");
+            assertEquals(null,m.getLearnedWord("bonjour"));           
+        }
+        
+        @Test
+        public void testLearnWordT(){
+            MarkovData m = new MarkovData("Il était une fois");
+            m.learnWord("dans", "la");
+            assertEquals("la",m.getLearnedWord("dans"));
+        }
+        
+        @Test
+        public void testLearnWordF(){
+            MarkovData m = new MarkovData("Il était une fois");
+            m.learnWord("dans", "la");
+            assertEquals(null,m.getLearnedWord("une"));
+        }
+        
+        @Test
+        public void testgetKeyWordT(){
+            MarkovData m = new MarkovData("Il était une fois");            
+            assertEquals("Il",m.getKeyWord(0));
+        }
+        
+        @Test
+        public void testKeyWordSize(){
+            MarkovData m = new MarkovData("Il était une fois");            
+            assertEquals(4,m.keyWordSize());
+        }
+        
+        
+        
+        
 }
--- src/test/java/com/cfranc/imple/markov/MarkovTest.java
+++ src/test/java/com/cfranc/imple/markov/MarkovTest.java
@@ -4,11 +4,33 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+//@author: Florian Sari
 public class MarkovTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
+	public void testDataFalse() {
+            Markov m = new Markov("Il était une fois");
+            assertEquals(false, m.data.containsKeyWord("fois"));
+        }
+        
+        @Test
+	public void testGenMark() {
+            Markov m = new Markov("Il était une fois");
+            assertEquals("était une", m.genMarkov(0));
+        }
+        @Test
+	public void testGenMarklearn() {
+            Markov m = new Markov("Il était une fois");
+            m.learn("dans une foret lointaine");
+            assertEquals("dans une", m.genMarkov(4));
+        }
+        
+        @Test
+        public void testGenMarkLongueChaine(){
+           Markov m = new Markov("Il était une fois dans un pays tres lointain"); 
+           assertEquals("dans un pays", m.genMarkov(5));
+        }
+
+        
 
 }
